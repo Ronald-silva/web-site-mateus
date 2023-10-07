c43a9e733f2a5cf6bb114d55aba05fa67215dccf
RONALD <118548856+Ronald-silva@users.noreply.github.com>
2023-10-07 17:04:53 -0300

    correcao ortografica

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
- let ul = document.querySelector('nav .ul');
+//  let ul = document.querySelector('nav .ul');
 
- function openMenu(){
-     ul.classList.add('open');
- }
- function closeMenu(){
-    ul.classList.remove('open');
- }
+//  function openMenu(){
+//      ul.classList.add('open');
+//  }
+//  function closeMenu(){
+//     ul.classList.remove('open');
+//  }
+var menuIcon = document.querySelector(".menu-icon");
+console.log(menuIcon);
+menuIcon.addEventListener(".menuIcon",function(){});
